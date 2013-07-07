e827032a511b88ab4b9136b78b42c4082a562993
barfoos <netzverweigerer@gmail.com>
2013-07-07 23:57:21 +0200

    fixed utf-8 bug with urltitle

--- urltitle/urltitle.py
+++ urltitle/urltitle.py
@@ -3,7 +3,9 @@
 import urllib2
 from BeautifulSoup import BeautifulSoup
 
-def urltitle(url):
+def urltitle(url,usermask,channel):
+  print "urltitle was called on: " + url
   title = BeautifulSoup(urllib2.urlopen(url,timeout=3)).title.string
+  title = str(title)
   return "::: " + title
 
